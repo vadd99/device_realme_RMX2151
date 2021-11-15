f1177a97dec5602c61f10595e18ab1fd05b5587f
vadd99 <92510965+vadd99@users.noreply.github.com>
2021-11-15 09:59:11 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
 rm -rf packages/apps/RealmeParts
-git clone https://github.com/Realme-G70-Series/android_packages_apps_RealmeParts packages/apps/RealmeParts
+git clone https://github.com/vadd99/android_packages_apps_RealmeParts packages/apps/RealmeParts
