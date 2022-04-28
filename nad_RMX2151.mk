72813af1050975b22ef4e6ec81358c083228438e
vadd99 <92510965+vadd99@users.noreply.github.com>
2022-04-28 18:15:58 +0700

    Update and rename banana_RMX2151.mk to nad_RMX2151.mk

--- nad_RMX2151.mk
+++ nad_RMX2151.mk
@@ -22,20 +22,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/RMX2151/device.mk)
 
 # Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/banana/config/common_full_phone.mk)
+$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
 
 TARGET_GAPPS_ARCH := arm64
 TARGET_BOOT_ANIMATION_RES := 1080
 CUSTOM_BUILD_TYPE=UNOFFICIAL
 TARGET_FACE_UNLOCK_SUPPORTED := true
 PRODUCT_PROPERTY_OVERRIDES += \
-ARCANE_DEVICE := aosp_RMX2151
-ARCANE_MAINTAINER := VADD
-ARCANE_GAPPED := true
-TARGET_SUPPORTS_BLUR := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := banana_RMX2151
+PRODUCT_NAME := nad_RMX2151
 PRODUCT_DEVICE := RMX2151
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G95 Series
